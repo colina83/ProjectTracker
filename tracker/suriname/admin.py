57bea124eb3833ae06b52585c2ba7d5889972377
Francisco.Colina <franco.collina@gmail.com>
2023-08-19 20:31:42 -0500

    Adding Models to DB

--- tracker/suriname/admin.py
+++ tracker/suriname/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Manager, Company, Project,Product,Status
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manager')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stage')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('project', 'status', 'date', 'company')
--- tracker/suriname/models.py
+++ tracker/suriname/models.py
@@ -1,11 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
 class Manager(models.Model):
-    first_name = models.CharField(max_length=20, null=True)
-
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    
     def __str__(self):
-        return 
+        return self.first_name + " " + self.last_name
 
-    def __unicode__(self):
-        return 
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    
+    STAGE = {
+        ('CP','Completed'),
+        ('PL', 'Planned'),
+        ('Ongoing', 'Ongoing'),
+    }
+           
+    name = models.CharField(max_length=30)    
+    stage = models.CharField(max_length=10, choices=STAGE)
+    products = models.ManyToManyField('Product', related_name='projects')
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    PRODUCTS_CHOICES = (
+        ('A', 'Acquisition Deliverables'),
+        ('FT', 'Fast-Track PSDM'),
+        ('FI', 'Full-Integrity PSDM'),
+        ('N', 'No Product')
+    )
+     
+    code = models.CharField(max_length=2, choices=PRODUCTS_CHOICES, primary_key=True)
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class Status(models.Model):
+    STATUS_CHOICES = (
+        ('DR', 'Data Review'),
+        ('P', 'Proposal'),
+        ('N', 'No Contact'),
+        ('C', 'Contacted'),  
+        ('L','Licensed')      
+    )
+    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    date = models.DateField(null=True, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.status
